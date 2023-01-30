b6946967809537a0bf82a7d5e92f4ad9745ca4b8
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2023-01-30 11:38:50 +0100

    Changes in index

--- index.jl
+++ index.jl
@@ -18,7 +18,7 @@ Julia is a general programming language suited for scientific computing applicat
 
 Hi! My name is Stefano Campanella, I work at [OGS](https://www.ogs.it/), I like Julia.
 
-> _He had a lot to say. He had a lot of nothing to say._ -- Eulogy, Ænima , Tool
+> _He had a lot to say. He had a lot of nothing to say._ -- Eulogy, Tool (Ænima, 1996)
 
 ## Seminar outline
 
@@ -43,17 +43,17 @@ Let's begin with a digression and answer the question _why should I learn progra
 md"""
 ## Motivation
 
-The great pyramid of Giza resisted the forces of nature, the winds, the sun, and the movements of the Earth, serving its purpose for more than four thousands years. Its thick walls might even provide a good energy rating, but nobody would ever immagine to make new buildings using the same solutions adopted in the ancient Egypt. This analogy illustrate why, in the opinion of the author, you should **stop writing scientific code in Fortran _whenever possible_**. Which language one might use then? 
+The great pyramid of Giza resisted the forces of nature, the winds, the sun, and the movements of the Earth, serving its purpose for more than four thousands years. Its thick walls might even provide good insulation, but nobody would ever immagine to make new buildings using the same solutions adopted in the ancient Egypt. This analogy illustrate why, in the opinion of the author, you should **stop writing scientific code in Fortran _whenever possible_**. Which language one might use then? 
 
-There are practical reasons why **Julia it's a good choice**: it's a language that allows to write code that is maintainable, correct and sufficiently fast to shortcircuit prototipation and production cycle (the so-called two language problem).
+There are practical reasons why **Julia it's a good choice**: it's a language that allows to easily write code that is maintainable, correct and sufficiently fast to shortcircuit prototipation and production cycle (the so-called _two language problem_).
 
-On a less technical level, Julia code tends to comply with the virtues proposed by Italo Calvino in "Six Memos for the Next Millennium": lightness, quickness, exactitude, visibility, multiplicity, and consistency (an unrecognized treatise about software engineering, in the end code is literature), more than the venerable FORTRAN 77.
+To put it more colorfully, Julia provides you the tools to write code that comply with the virtues proposed by Italo Calvino in "Six Memos for the Next Millennium": lightness, quickness, exactitude, visibility, multiplicity, and consistency--code is literature, and Calvino's lectures are an unrecognized treatise about software engineering. These tools (abstractions and facilities) are missing from the venerable FORTRAN 77, for example.
 
-On an even lesser technical level, and this is my favourite answer, Julia is a good choice because **writing Julia code is fun**. This is a perfectly reasonable argument to choose a programming language (or any other thing used to reason about computer programs and processes, the spirits that live in the computer). 
+On a less technical level, and this is my favourite answer, Julia is a good choice because **writing Julia code is fun**. This is a perfectly reasonable argument to choose a programming language, as long as you have freedom of choice.
 
 As in the quote of Alan J. Perlis that opens the memorable Structure and Interpretation of Computer Programs by Abelson and Sussmann:
 
->``I think that it's extraordinarily important that we in computer science keep fun in computing. When it started out, it was an awful lot of fun. Of course, the paying customers got shafted every now and then, and after a while we began to take their complaints seriously. We began to feel as if we really were responsible for the successful, error-free perfect use of these machines. I don't think we are. I think we're responsible for stretching them, setting them off in new directions, and keeping fun in the house. I hope the field of computer science never loses its sense of fun. Above all, I hope we don't become missionaries. Don't feel as if you're Bible salesmen. The world has too many of those already. What you know about computing other people will learn. Don't feel as if the key to successful computing is only in your hands. What's in your hands, I think and hope, is intelligence: the ability to see the machine as more than when you were first led up to it, that you can make it more.''
+>I think that it's extraordinarily important that we in computer science keep fun in computing. When it started out, it was an awful lot of fun. Of course, the paying customers got shafted every now and then, and after a while we began to take their complaints seriously. We began to feel as if we really were responsible for the successful, error-free perfect use of these machines. I don't think we are. I think we're responsible for stretching them, setting them off in new directions, and keeping fun in the house. I hope the field of computer science never loses its sense of fun. Above all, I hope we don't become missionaries. Don't feel as if you're Bible salesmen. The world has too many of those already. What you know about computing other people will learn. Don't feel as if the key to successful computing is only in your hands. What's in your hands, I think and hope, is intelligence: the ability to see the machine as more than when you were first led up to it, that you can make it more.
 """
 
 # ╔═╡ Cell order:
