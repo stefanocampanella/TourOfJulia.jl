cf655f5407bcb5955aa8fd7d6d5f5a0bf9c4ca80
Stefano Campanella <15182642+stefanocampanella@users.noreply.github.com>
2023-10-03 17:59:53 +0200

    Restructures directory tree and updates index.jl

---
+++ track 1/printing.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.29
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ d94b60b8-6204-11ee-1499-eb6de38abc4d
+println("hello!")
+
+# ╔═╡ Cell order:
+# ╠═d94b60b8-6204-11ee-1499-eb6de38abc4d
