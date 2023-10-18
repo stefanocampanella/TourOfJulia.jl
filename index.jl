1e18d6e13350f92eefe9cf515529dd517c350111
stefanocampanella <15182642+stefanocampanella@users.noreply.github.com>
2023-10-18 09:51:42 +0000

    Deploying to gh-pages from @ stefanocampanella/TourOfJulia.jl@a13e335791b3de9a104b460ff3e5b6374b1db010 🚀

--- index.jl
+++ index.jl
@@ -20,7 +20,11 @@ using PyCall, BenchmarkTools
 md"""
 # A tour of Julia
 
-Julia is a general purpose programming language suited for technical computing. It is a dynamic, high-level language, which means that executing a program written in Julia does not require the user to compile it beforehand, as for C or Fortran, and its syntax is similar to MATLAB or Python. Nonetheless, its performance is close to that of statically compiled languages. This is possible thanks to clever design, and Just In Time (JIT) compilation (as JavaScript/TypeScript in your browser or Python when using Numba).
+Julia is a general purpose programming language suited for technical and scientific computing. 
+
+It is a dynamic, high-level language, with performance close to statically compiled languages, like C or Fortran, while having a syntax similar to other interpreted languages, like MATLAB or Python. This is possible thanks to a clever design, and Just In Time (JIT) compilation.
+
+In the following I will introduce the language, its syntax and main features.
 """
 
 # ╔═╡ b26ee80f-d113-4c31-80ff-903d6f1b947b
@@ -47,13 +51,13 @@ begin
 		
 	The material covered in this seminar is hardly original. It is mostly a reworking of the [official Julia documentation](https://docs.julialang.org/en/v1/), which is extensive and well written, and which I will refer to on several occasions. 
 		
-	I will begin with some [preliminary stuff]($(root * "extra/prelims" * extension)) (installation, package management, editors and IDE support). After that, we will touch some topics from two tracks. Finally, we will see simple examples and applications showing features and putting together a few things seen along the way.
+	I will begin with some [preliminary stuff]($(root * "extra/prelims" * extension)) (installation, package management, editors and IDE support). After that, we will follow the first track and touch some topics taken from the second. Finally, we will see a few simple examples and applications, showing features and putting together a few things seen along the way.
 		
 	### Track 1
 	
 	The first track contains a more gentle introduction to programming in Julia, and requires only a limited coding experience.
 	
-	1. [Built-in numerical types]($(root * "track 1/built-ins" * extension))
+	1. [Built-in numerical types and missing values]($(root * "track 1/built-ins" * extension))
 	1. [Variables]($(root * "track 1/variables" * extension))
 	1. [Control flow]($(root * "track 1/controlflow" * extension))
 	1. [Multidimensional arrays]($(root * "track 1/arrays" * extension))
@@ -68,7 +72,7 @@ begin
 	
 	### Examples
 	
-	The examples have a varied degree of complexity and present the implementation of algorithms from numerical analysis and other fields.
+	The examples have a varied degree of complexity and present the implementation of of both classical and numerical algorithms.
 	
 	1. [Huffman coding]($(root * "examples/huffman" * extension))
 	1. [Two-dimensional Laplace equation]($(root * "examples/jacobi" * extension))
@@ -94,7 +98,7 @@ The great pyramid of Giza resisted the forces of nature, the winds, the sun, and
 
 Julia is a language that allows to easily write code that is correct[^1], maintainable, and sufficiently fast to shortcircuit prototipation and production cycle (the so-called _two language problem_).[^2]
 
-On a less technical level, **writing Julia code is fun**[^3] (as long as you have freedom of choice, this is a perfectly reasonable argument).
+On a less technical level, **writing Julia code is fun**[^3].
 
 Before moving on, I'll add a few words about expectations on Julia.
 
@@ -115,17 +119,17 @@ Before moving on, I'll add a few words about expectations on Julia.
 md"""
 ## Disclaimer
 
-Julia **will not make your code automagically fast**, and you could potentially write awfully slow Julia code. Nonetheless the language will make it easy to write efficient code. Also, it encourages the re-use of code, which is one of the most fundamental principles of software engineering
+Julia **will not make your code automagically fast**, and you could potentially write awfully slow Julia code. Nonetheless the language will make it easy to write efficient code. Also, it encourages the re-use of code, which is one of the most fundamental principles of software engineering.
 
-Julia is a **young programming language** (first public release in 2012, 1.0 release in 2018), and some tools and libraries might not be available. The package ecosystem is vast, but some packages might not have the maturity of the alternatives in other programming languages. 
+Julia is a **young programming language** (first public release in 2012, 1.0 release in 2018), and some tools and libraries might not be available. The package ecosystem is vast, but some packages might not have the maturity of the alternatives provided by other programming languages. 
 
-Finally, when choosing the programming language for your next project, you should take into account the effort of learning a new one, the man-months spent on existing codebase, and the expertise of the colleagues you collaborate with. Indeed, you should also consider that Julia requires more sensibility and inclination towards computer science in comparison with MATLAB or R,.
+Finally, when choosing the programming language for your next project, you should take into account the effort of learning a new language, the man-months spent on existing codebases, and the expertise and inclinations of the people you collaborate with. You should also consider that writing effective Julia code might require a deeper knowledge of the language than required by MATLAB or R.
 
-So in when Julia is a good choice?
+So, _in which cases is Julia a good choice?_
 
-One example is when you have to write **new algorithms from scratch**, when they cannot be written as array operations (i.e. with loops), and you want to avoid the clumsiness of verbose, low-level languages without loosing efficiency. 
+One example is when you have to write **new algorithms from scratch**, they cannot be written as array operations (i.e. you are forced to use explicit loops), and you want to avoid the clumsiness of verbose, low-level languages without loosing efficiency. 
 
-Another scenario might be **data-model integration using machine learning**: there will exist more feature-complete models written in Fortran, and the de facto standard for ML is Python, but writing an interface between the two is non-trivial. Julia solves the issue by having a single language for the two purposes.
+Another scenario might be **data-model integration using machine learning**: chances are that there will exist more stable or feature-complete models written in Fortran, and the de facto standard for ML is Python, but writing an interface between the two is non-trivial. Julia solves the issue by having a single language for the two purposes.
 """
 
 # ╔═╡ b0caa168-c18e-4f71-a4af-6dd3d6110b91
